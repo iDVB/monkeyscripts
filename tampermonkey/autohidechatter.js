bdd77693a9bbd00b5aa9b8543ca744493ab53db6
Dan Van Brunt <dvanbrunt@gmail.com>
2015-07-20 13:28:03 -0400

    first commit

---
+++ tampermonkey/autohidechatter.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         AutoHide Chatter
+// @namespace    http://danvanbrunt.com/
+// @version      0.1
+// @description  Auto hide the chatter panel on all task pages.
+// @author       Dan Van Brunt
+// @include      https://genome.klick.com/tickets/*
+// @match        https://genome.klick.com/tickets/*
+// @copyright 2015+, danvanbrunt.com
+// @grant        none
+// ==/UserScript==
+
+
+
+//chatter.toggleChatter();
+chatter.hideChatter(true);
+
